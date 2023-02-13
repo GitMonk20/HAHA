a37ec55f3cff8fb30ccb7657baec12aac1e8d2b0
GitMonk20 <ravishankarr2022@outlook.com>
2023-02-13 13:32:19 +0800

    pRACTICE

---
+++ src/test/java/nus/edu/sg/AppTest.java
@@ -0,0 +1,18 @@
+package nus.edu.sg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
