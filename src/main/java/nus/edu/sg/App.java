71dbb01b5ddd48ec67fc9ce9c77b23b1d605aaf7
GitMonk20 <ravishankarr2022@outlook.com>
2023-02-13 13:49:58 +0800

    sync hello

--- src/main/java/nus/edu/sg/App.java
+++ src/main/java/nus/edu/sg/App.java
@@ -7,9 +7,7 @@ public class TicTacToe {
   static char[][] board = new char[3][3];
   static char turn = 'X';
 
-  public static void main(String[] args) {
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; j++) {
+  
         board[i][j] = ' ';
       }
     }
